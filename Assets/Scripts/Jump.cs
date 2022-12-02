08a5442e23b8a482c99d8f77f53d9741e226d429
Clayvets <anamaria271127@gmail.com>
2022-12-01 22:04:42 -0500

    Doble Salto

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Jump : MonoBehaviour
@@ -10,6 +11,8 @@ public class Jump : MonoBehaviour
     private Rigidbody2D rb;
     public float fallMultiplayer = 2.5f;
     public float lowJumpMultiplayer = 2f;
+    public int count;
+    bool canJump = true;
     
     void Start()
     {
@@ -17,19 +20,32 @@ public class Jump : MonoBehaviour
     }
     private void Update()
     {
-        bool jump = Input.GetButton("Jump");
-        if (jump)
+        bool jump = Input.GetButtonDown("Jump");
+        if (canJump && count <= 1)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
+            if (jump)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
+                count ++;
+                
+            }
         }
-
+        
         if (rb.velocity.y < 0)
         {
-            rb.velocity += new Vector2(0, Physics2D.gravity.y * (fallMultiplayer - 1) * Time.deltaTime * 1) ;
+            rb.velocity += new Vector2(0, Physics2D.gravity.y * (fallMultiplayer - 1) * Time.deltaTime * 1); //REVISAR FUNCIONAMIENTO
         }
         else if (rb.velocity.y > 0 && !jump)
         {
-            rb.velocity += new Vector2(0, Physics2D.gravity.y * (fallMultiplayer - 1) * Time.deltaTime * 1) ;
+            rb.velocity += new Vector2(0, Physics2D.gravity.y * (fallMultiplayer - 1) * Time.deltaTime * 1);
         }
     }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            count = 0;
+        }
+    } 
+    
 }
